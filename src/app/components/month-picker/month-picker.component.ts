8b35cc70bbb7d08acf5fbb7c61d69a662a109d78
Kaashyap <dora.meka@gmail.com>
2024-02-19 20:15:53 +0530

    Add files via upload

--- src/app/components/month-picker/month-picker.component.ts
+++ src/app/components/month-picker/month-picker.component.ts
@@ -1,1 +1,24 @@
+// calendar.component.ts
 
+import { Component, EventEmitter, Output } from '@angular/core'
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './month-picker.component.html',
+  styleUrls: ['./month-picker.component.scss']
+})
+export class MonthPickerComponent {
+  selectedMonth!: Date;
+  dateTime = new Date();
+  @Output() dateChanged: EventEmitter<Date> = new EventEmitter<Date>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updateDaysInMonth(event: any) {
+    this.selectedMonth = event;
+    this.dateChanged.emit(this.selectedMonth);
+  }
+}
